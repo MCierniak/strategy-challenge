21f66e477df990c0f6da69c9a52521213f7ce5fb
Mateusz Cierniak <mat.cierniak@gmail.com>
2023-06-03 15:02:22 +0200

    Player map grid resources

--- src/player/grid.cpp
+++ src/player/grid.cpp
@@ -1,26 +1,51 @@
+// Strategy Challenge Project
+// Copyright (C) 2023 Mateusz Cierniak
+//
+// This file is part of Strategy Challenge Project.
+//
+// Strategy Challenge Project is free software: you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or (at your
+// option) any later version.
+//
+// Strategy Challenge Project is distributed in the hope that it will be
+// useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// Strategy Challenge Project. If not, see <http://www.gnu.org/licenses/>.
+
 #include "grid.h"
+
+int emptySpace::resourceCount = 0;
 
 bool gridObj::checkTrav()
 {
     return isTraversable;
 }
 
-int gridObj::checkVal()
+int emptySpace::checkVal()
 {
     return value;
 }
 
-void gridObj::addUnit(std::unique_ptr<Unit> &newUnitPtr)
+void emptySpace::addUnit(std::unique_ptr<Unit> &newUnitPtr)
 {
     units.push_back(std::move(newUnitPtr));
 }
 
-void gridObj::setTrav(bool newTrav)
+void emptySpace::setTrav(bool newTrav)
 {
     isTraversable = newTrav;
 }
 
-void gridObj::removeUnit(int pos)
+void emptySpace::setAttack(bool newAtt)
+{
+    attackAdvantage = newAtt;
+}
+
+void emptySpace::removeUnit(std::size_t pos)
 {
     if (pos < units.size())
     {
@@ -32,35 +57,77 @@ void gridObj::removeUnit(int pos)
     }
 }
 
-void gridObj::setVal(int newVal)
+void emptySpace::setResource(bool newRes)
+{
+    resource = newRes;
+}
+
+void emptySpace::setVal(int newVal)
 {
     value = newVal;
 }
 
+int emptySpace::getResourceCount()
+{
+    return emptySpace::resourceCount;
+}
+
 std::string emptySpace::print()
 {
-    return std::string("empty");
+    if (resource)
+    {
+        if (units.size() == 0)
+        {
+            return std::string("r");
+        }
+        else
+        {
+            return std::string("r(units)");
+        }
+    }
+    else
+    {
+        if (units.size() == 0)
+        {
+            return std::string("e");
+        }
+        else
+        {
+            return std::string("e(units)");
+        }
+    }
 }
 
 std::string barrier::print()
 {
-    return std::string("barrier");
+    return std::string("b");
 }
 
-gridObj::gridObj(int val, bool traversible):
-    value(val), isTraversable(traversible)
+gridObj::gridObj(bool traversible):
+    isTraversable(traversible)
 {}
 
 gridObj::~gridObj(){}
 
 emptySpace::emptySpace():
-    gridObj(1, true)
-{}
+    gridObj(true), attackAdvantage(false), resource(false), value(1)
+{
+    if (resource)
+    {
+        emptySpace::resourceCount++;
+    }
+}
 
-emptySpace::~emptySpace(){}
+emptySpace::~emptySpace()
+{
+    if (resource)
+    {
+        emptySpace::resourceCount--;
+    }
+}
 
 barrier::barrier():
-    gridObj(1000000, false)
+    gridObj(false)
 {}
 
 barrier::~barrier(){}
--- src/player/grid.h
+++ src/player/grid.h
@@ -1,3 +1,21 @@
+// Strategy Challenge Project
+// Copyright (C) 2023 Mateusz Cierniak
+//
+// This file is part of Strategy Challenge Project.
+//
+// Strategy Challenge Project is free software: you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or (at your
+// option) any later version.
+//
+// Strategy Challenge Project is distributed in the hope that it will be
+// useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// Strategy Challenge Project. If not, see <http://www.gnu.org/licenses/>.
+
 #ifndef GRID_H
 #define GRID_H
 
@@ -7,39 +25,66 @@
 #include <vector>
 #include <string>
 
+// Vector for map tiles. Call with grid[y coord][x coord]
 #define grid_row std::vector<std::unique_ptr<gridObj>>
-#define grid std::vector<grid_row>                      // call with grid[y coord][x coord]
+#define grid std::vector<grid_row>
 
+// Abstract interface for grid tiles
 class gridObj
 {
-private:
+protected:
+    // Bool indicating if unit can stop on this tile
     bool isTraversable;
-    unitRoster units;
-    int value;
 public:
-    gridObj(int val, bool traversible);
+    gridObj(bool traversible);
     ~gridObj();
 
     virtual std::string print() = 0;
     
+    // Check if unit can stop on this node
     bool checkTrav();
-    int checkVal();
-
-    void addUnit(std::unique_ptr<Unit> &newUnitPtr);
-    void setTrav(bool newTrav);
-    void removeUnit(int pos);
-    void setVal(int newVal);
 };
 
+// Empty space tile and also resource tile class
 class emptySpace : public gridObj
 {
+protected:
+    // Logic flags for decision making
+    bool attackAdvantage, resource;
+    // Vector of units on the tile
+    unitRoster units;
+    // Tile value for decision making
+    int value;
+
+    // Static count of resource tiles
+    static int resourceCount;
 public:
     emptySpace();
     ~emptySpace();
 
     std::string print();
+
+    // Check tile value
+    int checkVal();
+
+    // Add unit to tile
+    void addUnit(std::unique_ptr<Unit> &newUnitPtr);
+    // Remove unit from tile roster. O(1)
+    void removeUnit(std::size_t pos);
+    // Change tile status from empty to resource
+    void setResource(bool newRes);
+    // Set traversal status
+    void setTrav(bool newTrav);
+    // Set attack advantage status
+    void setAttack(bool newAtt);
+    // Set tile value
+    void setVal(int newVal);
+
+    // Get current resource node count
+    static int getResourceCount();
 };
 
+// Blockade class
 class barrier : public gridObj
 {
 public:
